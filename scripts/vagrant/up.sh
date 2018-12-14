0eeda350a81dc3653e629a30fe51c5775d59bd1a
Kaspars Dambis <hi@kaspars.net>
2018-12-14 15:13:52 +0200

    Support both yml and yaml config files

--- scripts/vagrant/up.sh
+++ scripts/vagrant/up.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose -f /vagrant/docker-compose.yml up -d
+cd /vagrant
+
+docker-compose up -d
+
 echo "Done! Please wait a few seconds before the containers are up."
